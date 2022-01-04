b2ad005be571fd1e6308843749d03e2ce6c99512
khalil-Elf441 <khalil.el-fallahi@alumni.univ-avignon.fr>
2022-01-04 22:54:09 +0100

    Code Review and Commentes

--- y/actions/actions.py
+++ y/actions/actions.py
@@ -5,8 +5,6 @@
 # https://rasa.com/docs/rasa/custom-actions
 
 
-# This is a simple example for a custom action which utters "Hello World!"
-
 from typing import Any, Text, Dict, List
 import requests
 from rasa_sdk import Action, Tracker
@@ -14,12 +12,19 @@ from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
 import json
 import pprint
+import requests
 
-places_db = {}
+
+
 
 YELP_BUSSINESS_SEARCH_LINK = 'https://api.yelp.com/v3/businesses/search'
 YELP_BUSSINESS_DETAILS_LINK = "https://api.yelp.com/v3/businesses/"
+QrCodeApi = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
 
+# temp place db / Deprecated
+places_db = {}
+
+# First implemented version of YELP
 class ActionCheckRestaurants(Action):
 
     def name(self) -> Text:
@@ -29,13 +34,9 @@ class ActionCheckRestaurants(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-
+        # Send reauest to YELP
         auth_token = 'NgF35-znpIaEKTTtAlOqdtY_iBoXM7XnRo2qaYY1uXlyCga7-hltIEGO-qtUsdzAS8ks8VXUBUsU-a22Tqc4Dn3LmOkp0smZH-sTzSFovpYr-xnLeCfshtwM2yC2YXYx'
         hed = {'Authorization': 'Bearer ' + auth_token}
-
-        # data = '{"location": "paris", "term": "restaurant"}'
-
-
 
         data = {}
         data['location'] = 'avignon'
@@ -45,34 +46,21 @@ class ActionCheckRestaurants(Action):
         url = 'https://api.yelp.com/v3/businesses/search'
      
         response = requests.get(url, params=json.loads(json_data), headers=hed)
-        #  print(response)
-        #  print(response.json())
 
         data = response.json()
-        # for res in data['businesses']:
-        #  print(res['name'])
-        
 
         # fill db places :
         for e in data['businesses']:
          places_db[str(data['businesses'].index(e))] = str(e['name'])
-        
-        # # return only one restaurant ####### Last robot test
-        # dispatcher.utter_message(text="the restaurant is "+data['businesses'][0]['name'])
-        # return []
-
-        # group all businesses_places in one String
-        # businesses_places = '\n '.join(str(data['businesses'].index(e))+" - "+str(e['name']) for e in data['businesses'])
 
         businesses_places = ' '.join(str(data['businesses'].index(e))+" "+str(e['name']) for e in data['businesses'])
 
         businesses_places = "here are the restaurants around \n "+businesses_places
         dispatcher.utter_message(text=businesses_places)
-        # return [SlotSet("businesses_places", businesses_places)]
         return []
 
 
-#  Deprecated
+#  Deprecated version of get Itenerary
 class ActionGetItinerary(Action):
 
     def name(self) -> Text:
@@ -105,30 +93,15 @@ class ActionGetItinerary(Action):
         import json
 
         response = requests.get(CompletQrCodeLink)
-        # print(response)
 
-        # download image
-        # img_data = requests.get(CompletQrCodeLink).content
-
-        # with open('QRCode.jpg', 'wb') as handler:
-        #     handler.write(img_data)
-
-        # print(response.json())
-        # data = response.json()
-        # print(data)
-        # for res in data['businesses']:
-        #  print(res['name'])
-        # dispatcher.utter_message(text=data, image = image)
-
-        # utter_message(image=<image url>)
-
-        # msg = f"find the itinerary from this link {response} or scan this QR Code"
         dispatcher.utter_message(text=msg)
         dispatcher.utter_message(image=response)
 
         return []
 
 
+
+# Get the Itinerary from index / first version 
 class ActionGetItineraryFromIndex(Action):
 
     def name(self) -> Text:
@@ -138,11 +111,11 @@ class ActionGetItineraryFromIndex(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        import requests
 
+        # QRCODE QPI
         QrCodeApi = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
 
-        # Ceri Adress
+        # Ceri Adress by default
         default_starting_place_ceri = "339 Chem. des Meinajaries, 84000 Avignon"
 
         destination_place_index = next(
@@ -160,36 +133,23 @@ class ActionGetItineraryFromIndex(Action):
             return []
 
         msg = f"Your destination is to the restaurant : {name_of_place}"
-        # dispatcher.utter_message(text=msg)
-        # return []
-
-        # destination = "2 Impasse de l'Epi, 84000 Avignon"
         default_city_destination = "84000 Avignon, France"
         GoogleMapsItinerary = "https://www.google.fr/maps/dir/" + default_starting_place_ceri+"/"+str(name_of_place)+" "+default_city_destination+"/"
 
         CompletQrCodeLink = QrCodeApi+GoogleMapsItinerary
-        # import json
         response = requests.get(CompletQrCodeLink)
-        # print(response)
-        # print(response.json())
-        # data = response.json()
-        # print(data)
-        # for res in data['businesses']:
-        #  print(res['name'])
-        # dispatcher.utter_message(text=data, image = image)
 
+        # display CodeQR as link
         # utter_message(image=<image url>)
         msg = msg + " \n " + "find the itinerary from [this link]("+CompletQrCodeLink+") or scan this QR Code"
         dispatcher.utter_message(text=msg)
         # dispatcher.utter_message(text=msg)
         # dispatcher.utter_image_url(image=response)
         # dispatcher.utter_message(image=CompletQrCodeLink)
-
         return []
 
 
-# action_get_restaurant_from_index
-
+# get restaunrants from index / deprecated / restaurants as changed by term to have many options 
 class ActionGetRestaurantFromIndex(Action):
 
     def name(self) -> Text:
@@ -218,9 +178,10 @@ class ActionGetRestaurantFromIndex(Action):
         return []
 
 
-
+# list of possible term choices / sync with nlu
 parents = ['restaurants','hotels','cafe','bars']
 
+# Propose categories based on the user term
 class ActionGetCategoriesFromTerm(Action):
 
     def name(self) -> Text:
@@ -230,9 +191,7 @@ class ActionGetCategoriesFromTerm(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        # auth_token = 'NgF35-znpIaEKTTtAlOqdtY_iBoXM7XnRo2qaYY1uXlyCga7-hltIEGO-qtUsdzAS8ks8VXUBUsU-a22Tqc4Dn3LmOkp0smZH-sTzSFovpYr-xnLeCfshtwM2yC2YXYx'
-        # head = {'Authorization': 'Bearer ' + auth_token}
-
+        # Get the term
         user_term = next(tracker.get_latest_entity_values("term"), None)     
         categories = []
 
@@ -249,7 +208,6 @@ class ActionGetCategoriesFromTerm(Action):
         
 
         #  get categories from neraby approch
-
         auth_token = 'NgF35-znpIaEKTTtAlOqdtY_iBoXM7XnRo2qaYY1uXlyCga7-hltIEGO-qtUsdzAS8ks8VXUBUsU-a22Tqc4Dn3LmOkp0smZH-sTzSFovpYr-xnLeCfshtwM2yC2YXYx'
         head = {'Authorization': 'Bearer ' + auth_token}
 
@@ -264,18 +222,12 @@ class ActionGetCategoriesFromTerm(Action):
         for term in data['businesses']:
             for term_categories_res in term["categories"]:
                 if term_categories_res not in parents:
+                    # get the alias / the name of the term in YELP API
                     categories.append(term_categories_res['alias'])
         
-
-
-        #  print(response)
-        # pprint.pprint(response.json())
-
-        # categories = "test"
-        # print(response.json())
-        # data = response.json()
         categories = list(set(categories))
 
+        # group all the categories options in one String
         categories_as_string = ' or '.join(
             list(
                 set(
@@ -284,22 +236,14 @@ class ActionGetCategoriesFromTerm(Action):
             )[:5]
             )
 
-        # print(categories)
         msg = f"which fron the {user_term} s categories you prefer {categories_as_string} ?"
         dispatcher.utter_message(text=msg)
-        # , SlotSet("Key2", "Value2")
-
-        # for key in slot_keys_values.keys():
-        #     slot = SlotSet(key, slot_keys_values[key])
-        #     slots.append(slot)
-        # return slots
-        # , SlotSet("term", user_term)
         return [SlotSet("term_categories", categories)]
 
-
+# temp bussness places / Deprecated
 businesses_places_global = []
 
-
+# EXtract the category and propose it to the user
 class ActionGetUserCategoryChoice(Action):
 
     def name(self) -> Text:
@@ -312,11 +256,11 @@ class ActionGetUserCategoryChoice(Action):
         # user_category_choice = next(tracker.get_slot("category"), None)
         user_category_choice = next(tracker.get_latest_entity_values("category"), None)
 
-        print(user_category_choice)
+        # print(user_category_choice)
 
         List_categories = tracker.get_slot("term_categories")
 
-        print(List_categories)
+        # print(List_categories)
 
         if not user_category_choice:
             msg = f"I didnt recognize {user_category_choice}. Can you try again ?"
@@ -365,17 +309,15 @@ class ActionGetUserCategoryChoice(Action):
 
             print(businesses_places_as_string)
 
-            # for key in slot_keys_values.keys():
-            #     slot = SlotSet(key, slot_keys_values[key])
-            #     slots.append(slot)
-            # return slots
-
+            # propose catogies to the user
             msg = f"Here is the {saved_term} with this category {businesses_places_as_string} What term's number will you choose? "
             dispatcher.utter_message(text=msg)
             return [SlotSet("term_category", user_category_choice)]    
 
+# temp varible to store the place details
 target_term_data_details = ""
 
+# Extract the the user term choice / the term is a name exemple hotel ibis
 class ActionPointTerm(Action):
 
     def name(self) -> Text:
@@ -394,6 +336,7 @@ class ActionPointTerm(Action):
             dispatcher.utter_message(text=msg)
             return[]
 
+        # concertion index from name to number
         if term_index == "one":
             term_index = "1"
         
@@ -414,7 +357,7 @@ class ActionPointTerm(Action):
 
         targetId = ""
         data = ""
-        # find the id oh the term chose by user
+        # find the id of the term chose by user
         if businesses_places_global:
             print(businesses_places_global)
             for indexId in businesses_places_global:
@@ -424,7 +367,7 @@ class ActionPointTerm(Action):
                     targetId = id
                     break
             
-            print(f"target Id {targetId}")
+            # print(f"target Id {targetId}")
 
             # launch get request to the endpoint bussiness id
             auth_token = 'NgF35-znpIaEKTTtAlOqdtY_iBoXM7XnRo2qaYY1uXlyCga7-hltIEGO-qtUsdzAS8ks8VXUBUsU-a22Tqc4Dn3LmOkp0smZH-sTzSFovpYr-xnLeCfshtwM2yC2YXYx'
@@ -434,15 +377,16 @@ class ActionPointTerm(Action):
             target_term_data_details = response.json()
             data = response.json()
 
-            print("--> target_term_data_details")
-            print(target_term_data_details)
+            # print("--> target_term_data_details")
+            # print(target_term_data_details)
 
         msg = "Now that I know your favorite restaurant I can give you more informations"
         dispatcher.utter_message(text=msg)
+        # set the hotel id to have many details in the previous actions from YELP BUSSINESS DETAILS
         return [SlotSet("target_term_id", data["id"])]
 
-
-class ActionHelloWorld(Action):
+# give the user the contact information of the term 
+class ActionGetContact(Action):
 
     def name(self) -> Text:
         return "action_get_contact"
@@ -451,12 +395,12 @@ class ActionHelloWorld(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
-        print("target_term_data_details <--")
-        print(target_term_data_details)
+        # print("target_term_data_details <--")
+        # print(target_term_data_details)
 
         requested_term_id = tracker.get_slot("target_term_id")
 
-        print(requested_term_id)
+        # print(requested_term_id)
 
         if requested_term_id is not None:
             auth_token = 'NgF35-znpIaEKTTtAlOqdtY_iBoXM7XnRo2qaYY1uXlyCga7-hltIEGO-qtUsdzAS8ks8VXUBUsU-a22Tqc4Dn3LmOkp0smZH-sTzSFovpYr-xnLeCfshtwM2yC2YXYx'
@@ -478,8 +422,8 @@ class ActionHelloWorld(Action):
         dispatcher.utter_message(text="ups you should answer some questions before coming to this information")
         return []
 
-
-class ActionHelloWorld(Action):
+#  give the user the term rating from ! to 5 starts depends on yelp DataBase
+class ActionGetRating(Action):
 
     def name(self) -> Text:
         return "action_get_rating"
@@ -510,7 +454,7 @@ class ActionHelloWorld(Action):
             text="ups you should answer some questions before coming to this information")
         return []
 
-
+# genere QrCode itinerary to the place
 class ActionGetItineraryFromIndex(Action):
 
     def name(self) -> Text:
@@ -520,13 +464,9 @@ class ActionGetItineraryFromIndex(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        import requests
-
-        QrCodeApi = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
-
         # Ceri Adress
         default_starting_place_ceri = "339 Chem. des Meinajaries, 84000 Avignon"
-
+        # term ID
         requested_term_id = tracker.get_slot("target_term_id")
 
         if not requested_term_id:
@@ -534,7 +474,7 @@ class ActionGetItineraryFromIndex(Action):
             dispatcher.utter_message(text=msg)
             return []
         
-
+        #  launch request for the term DEtails
         auth_token = 'NgF35-znpIaEKTTtAlOqdtY_iBoXM7XnRo2qaYY1uXlyCga7-hltIEGO-qtUsdzAS8ks8VXUBUsU-a22Tqc4Dn3LmOkp0smZH-sTzSFovpYr-xnLeCfshtwM2yC2YXYx'
         head = {'Authorization': 'Bearer ' + auth_token}
         term_details_ylp_link = YELP_BUSSINESS_DETAILS_LINK + requested_term_id
@@ -542,12 +482,7 @@ class ActionGetItineraryFromIndex(Action):
         data = response.json()
         name_of_place = data["name"]
 
-        print(name_of_place)
-
-        # if not destination_place_index:
-        #     msg = f"I didnt recognize {name_of_place} .is it spelled correctly?"
-        #     dispatcher.utter_message(text=msg)
-        #     return []
+        # print(name_of_place)
 
         msg = f"Your destination is to the restaurant : {name_of_place}"
 
@@ -555,33 +490,27 @@ class ActionGetItineraryFromIndex(Action):
         # dispatcher.utter_message(text=msg)
         # return []
 
-        print("Place Adress "+place_adress)
+        # print("Place Adress "+place_adress)
 
         # destination = "2 Impasse de l'Epi, 84000 Avignon"
         default_city_destination = " 84000 Avignon, France"
-
+        
+        #  generate the itinerary based on google maps 
         GoogleMapsItinerary = "https://www.google.fr/maps/dir/" + \
             default_starting_place_ceri+"/" + \
             str(place_adress)+" "+default_city_destination+"/"
 
         CompletQrCodeLink = QrCodeApi+GoogleMapsItinerary
-        # import json
         response = requests.get(CompletQrCodeLink)
-        # print(response)
-        # print(response.json())
-        # data = response.json()
-        # print(data)
-        # for res in data['businesses']:
-        #  print(res['name'])
-        # dispatcher.utter_message(text=data, image = image)
 
+        # give the destination with Qrcode link
         # utter_message(image=<image url>)
         msg = msg + " \n " + "find the itinerary from [this link]("+CompletQrCodeLink+") or scan this QR Code"
         dispatcher.utter_message(text=msg)
 
         return []
 
-#
+# default example
 #
 # class ActionHelloWorld(Action):
 #
